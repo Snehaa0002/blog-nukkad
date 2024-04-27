4e9237da9d45193b65bd3cf2b70e32eaf7f2f443
Snehaa0002 <163520243+Snehaa0002@users.noreply.github.com>
2024-04-27 21:27:38 +0530

    aboutus code

--- frontend/src/app/(main)/about/page.jsx
+++ frontend/src/app/(main)/about/page.jsx
@@ -1,2 +1,19 @@
+// AboutUsPage.js
+import React from 'react';
+
+const AboutUsPage = () => {
+  return (
+    <div className="about-us">
+      <h2>About Us</h2>
+      <p>Welcome to Blog Nukkad, your go-to place for all things blogging!</p>
+      <p>We are passionate about providing valuable content to our readers...</p>
+      <p>Contact us at: example@email.com</p>
+    </div>
+  );
+}
+
+export default AboutUsPage;
+
+
 
 
