adebe31d51ed3a1eb434ac8a57b4fbdf7f43a295
Snehaa0002 <163520243+Snehaa0002@users.noreply.github.com>
2024-04-29 15:08:06 +0530

    login

--- frontend/src/app/(main)/signup/page.jsx
+++ frontend/src/app/(main)/signup/page.jsx
@@ -6,7 +6,7 @@ function Signup() {
       <div className="mt-7 bg-white  border border-gray-200 rounded-xl shadow-sm dark:bg-neutral-900 dark:border-neutral-700">
         <div className="p-4 sm:p-7">
           <div className="text-center">
-            <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
+           <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
               Sign up
             </h1>
             <p className="mt-2 text-sm text-gray-600 dark:text-neutral-400">
