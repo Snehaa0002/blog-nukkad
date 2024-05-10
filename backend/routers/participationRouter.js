ac938374515d4ede8482518589005eee284695d3
m-triple-m <triplem656@gmail.com>
2024-05-10 15:43:54 +0530

    ..

---
+++ backend/routers/participationRouter.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const router = express.Router();
+const Model = require('../Models/participateModel');
+const verifyToken = require('./verifyToken');
+
+router.post('/add', verifyToken, (req, res) => {
+    req.body.user = req.user._id;
+    console.log(req.body);
+    new Model(req.body).save()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/getall', (req, res) => {
+    Model.find().populate('user')
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// : denotes url parameter
+
+router.delete('/delete/:id', (req, res) => {
+    Model.findByIdAndDelete(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/getbyid/:id', (req, res) => {
+    Model.findById(req.params.id).populate('user')
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/getbycompetition/:id', (req, res) => {
+    Model.find({ competition: req.params.id }).populate('user').populate('blog')
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/check-participation/:id', verifyToken, (req, res) => {
+    Model.findOne({ competition: req.params.id, user: req.user._id })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/getbyuser', verifyToken, (req, res) => {
+    Model.findOne({ user: req.user._id })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/update/:id', (req, res) => {
+    Model.findByIdAndUpdate(req.params.id, req.body).populate('user')
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
