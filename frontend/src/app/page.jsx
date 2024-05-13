3bfb34e25e80d0dcc62266a7955bb031248e0037
Snehaa0002 <163520243+Snehaa0002@users.noreply.github.com>
2024-05-13 11:10:22 +0530

    home

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+ import React from 'react'
 import Navbar from './(main)/navbar'
 
 const Home = () => {
@@ -67,138 +67,13 @@ const Home = () => {
         {/* End Hero */}
       </>
       <>
-      <>
-  {/* Card Blog */}
-  <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-    {/* Title */}
-    <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-      <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
-        Customer stories
-      </h2>
-      <p className="mt-1 text-gray-600 dark:text-neutral-400">
-        See how game-changing companies are making the most of every engagement
-        with Preline.
-      </p>
-    </div>
-    {/* End Title */}
-    {/* Grid */}
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      {/* Card */}
-      <a
-        className="group hover:bg-gray-100 rounded-xl p-5 transition-all dark:hover:bg-white/10"
-        href="#"
-      >
-        <div className="aspect-w-16 aspect-h-10">
-          <img
-            className="w-full object-cover rounded-xl"
-            src="https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
-            alt="Image Description"
-          />
-        </div>
-        <h3 className="mt-5 text-xl text-gray-800 dark:text-neutral-300 dark:hover:text-white">
-          Unity’s inside sales team drives 80% of its revenue with Preline.
-        </h3>
-        <p className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold text-gray-800 dark:text-neutral-200">
-          Learn more
-          <svg
-            className="flex-shrink-0 size-4 transition ease-in-out group-hover:translate-x-1"
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="m9 18 6-6-6-6" />
-          </svg>
-        </p>
-      </a>
-      {/* End Card */}
-      {/* Card */}
-      <a
-        className="group hover:bg-gray-100 rounded-xl p-5 transition-all dark:hover:bg-white/10"
-        href="#"
-      >
-        <div className="aspect-w-16 aspect-h-10">
-          <img
-            className="w-full object-cover rounded-xl"
-            src="https://images.unsplash.com/photo-1669739432571-aee1f057c41f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1065&q=80"
-            alt="Image Description"
-          />
-        </div>
-        <h3 className="mt-5 text-xl text-gray-800 dark:text-neutral-300 dark:hover:text-white">
-          Living Spaces creates a unified experience across the customer
-          journey.
-        </h3>
-        <p className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold text-gray-800 dark:text-neutral-200">
-          Learn more
-          <svg
-            className="flex-shrink-0 size-4 transition ease-in-out group-hover:translate-x-1"
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="m9 18 6-6-6-6" />
-          </svg>
-        </p>
-      </a>
-      {/* End Card */}
-      {/* Card */}
-      <a
-        className="group hover:bg-gray-100 rounded-xl p-5 transition-all dark:hover:bg-white/10"
-        href="#"
-      >
-        <div className="aspect-w-16 aspect-h-10">
-          <img
-            className="w-full object-cover rounded-xl"
-            src="https://images.unsplash.com/photo-1657299171054-e679f630a776?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-            alt="Image Description"
-          />
-        </div>
-        <h3 className="mt-5 text-xl text-gray-800 dark:text-neutral-300 dark:hover:text-white">
-          Atlassian powers sales and support at scale with Preline.
-        </h3>
-        <p className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold text-gray-800 dark:text-neutral-200">
-          Learn more
-          <svg
-            className="flex-shrink-0 size-4 transition ease-in-out group-hover:translate-x-1"
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="m9 18 6-6-6-6" />
-          </svg>
-        </p>
-      </a>
-      {/* End Card */}
-    </div>
-    {/* End Grid */}
-  </div>
-  {/* End Card Blog */}
-</>
-
         {/* Features */}
         <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
     
           <div className="aspect-w-10 aspect-h-2">
             <img
               className="w-full object-cover rounded-xl"
-              src="https://img.freepik.com/premium-vector/illustration-vector-graphic-cartoon-character-blogging_516790-1481.jpg"
+              src="https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2022/03/what-is-a-blog-1.webp"
             />
           </div>
       
