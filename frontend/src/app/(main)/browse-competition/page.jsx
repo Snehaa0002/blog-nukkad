c16a674e2fa085ba907bc7bdf14b4c0792cfd179
Snehaa0002 <163520243+Snehaa0002@users.noreply.github.com>
2024-06-10 10:57:21 +0530

    participant

--- frontend/src/app/(main)/browse-competition/page.jsx
+++ frontend/src/app/(main)/browse-competition/page.jsx
@@ -53,8 +53,10 @@ const Browse = () => {
                     >
                         View Details
                     </Link>
+                    
                 </div>
-            </div>
+                </div>
+        
         ))
     }
 
